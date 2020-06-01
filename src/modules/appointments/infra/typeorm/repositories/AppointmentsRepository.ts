56f1a44d193b8329c560780fef8cde38b28e36dd
vinicius <viniciusflores379@gmail.com>
2020-06-01 21:07:28 +0000

    :boom: create interface to repositorie

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,16 @@
 import { EntityRepository, Repository } from 'typeorm'
-
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentRepository'
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
 
 @EntityRepository(Appointment)
-class AppointmentsRepositories extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepositories extends Repository<Appointment>
+  implements IAppointmentRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     })
 
-    return findAppointment || null
+    return findAppointment
   }
 }
 
