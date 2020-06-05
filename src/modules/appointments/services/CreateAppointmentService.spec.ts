f7f6093a193c4e7b4edc0dc544be1f1ceaf0c272
vinicius <viniciusflores379@gmail.com>
2020-06-05 22:51:19 +0000

    :white_check_mark: complete appointment service test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,14 +1,15 @@
 import FakeAppointmentsRepositories from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
+import AppError from '@shared/errors/AppError'
 
 describe('CreateAppointment', () => {
   it('Should be able to create a new appointment', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepositories()
-    const createAppointmentService = new CreateAppointmentService(
+    const createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
     )
 
-    const appointment = await createAppointmentService.execute({
+    const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123',
     })
@@ -17,5 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123')
   })
 
-  // it('Should not be able to create two appointments on the same time', () => {})
+  it('Should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepositories()
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    )
+
+    const appointmentDate = new Date(2020, 5, 6, 10)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123',
+    })
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
 })
