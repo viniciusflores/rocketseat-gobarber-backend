35e990c4afefe02dfcfb2142962c3427b7d00c15
vinicius <viniciusflores379@gmail.com>
2020-06-05 21:05:26 +0000

    :white_check_mark: create first test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3)
+import FakeAppointmentsRepositories from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
+
+describe('CreateAppointment', () => {
+  it('Should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepositories()
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    )
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '123',
+    })
+
+    expect(appointment).toHaveProperty('id')
+    expect(appointment.provider_id).toBe('123')
+  })
+
+  // it('Should not be able to create two appointments on the same time', () => {})
 })
