81597ca84018584feba9616b015e6ac85389304f
vinicius <viniciusflores379@gmail.com>
2020-06-07 07:15:57 +0000

    :sparkles: Implement Template Provider

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO'
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider'
 
 export default class FakeEmailTemplateProvider
   implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template
+  public async parse(): Promise<string> {
+    return 'mail content'
   }
 }
