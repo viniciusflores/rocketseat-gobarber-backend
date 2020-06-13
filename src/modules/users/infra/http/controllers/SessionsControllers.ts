f79a7e819310f6f0681eaace33f533511016895c
vinicius <viniciusflores379@gmail.com>
2020-06-13 23:55:10 +0000

    :lock: include class transform

--- src/modules/users/infra/http/controllers/SessionsControllers.ts
+++ src/modules/users/infra/http/controllers/SessionsControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
+import { classToClass } from 'class-transformer'
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService'
 
 export default class SessionsControllers {
@@ -13,7 +14,6 @@ export default class SessionsControllers {
       password,
     })
 
-    delete user.password
-    return response.json({ user, token })
+    return response.json({ user: classToClass(user), token })
   }
 }
