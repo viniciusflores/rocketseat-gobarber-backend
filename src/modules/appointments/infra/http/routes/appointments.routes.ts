4c05b33847a28aad054a921471a4b381f8f94918
vinicius <viniciusflores379@gmail.com>
2020-06-13 19:50:39 +0000

    :sparkles: create listProviderappointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController'
+import ProvidersAppointmentsControllers from '@modules/appointments/infra/http/controllers/ProvidersAppointmentsControllers'
 
 const appointmentsRouter = Router()
 const appointmentsController = new AppointmentsController()
+const providersAppointmentsControllers = new ProvidersAppointmentsControllers()
 
 appointmentsRouter.use(ensureAuthenticated)
 
 appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.get('/me', providersAppointmentsControllers.index)
 
 export default appointmentsRouter
