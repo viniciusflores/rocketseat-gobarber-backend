d17b348138420f0e488204130163f03146b4e528
vinicius <viniciusflores379@gmail.com>
2020-06-14 06:29:55 +0000

    :construction: WIP with cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -4,6 +4,7 @@ import User from '@modules/users/infra/typeorm/entities/User'
 import AppError from '@shared/errors/AppError'
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider'
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 interface IRequest {
   name: string
@@ -16,6 +17,7 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
     @inject('HashProvider') private hashprovider: IHashProvider,
+    @inject('CacheProvider') private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -32,6 +34,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     })
+
+    await this.cacheProvider.invalidatePrefix(`providers-list:*`)
 
     return user
   }
