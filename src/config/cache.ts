049fb1fded045f666a4a0a5a69bc63e66aaefc47
vinicius <viniciusflores379@gmail.com>
2020-06-14 08:12:07 +0000

    :sparkles: Add rate limiter on routes

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -2,7 +2,6 @@ import { RedisOptions } from 'ioredis'
 
 interface ICacheConfig {
   driver: 'redis'
-
   config: {
     redis: RedisOptions
   }
@@ -10,12 +9,11 @@ interface ICacheConfig {
 
 export default {
   driver: 'redis',
-
   config: {
     regis: {
-      host: 'localhost',
-      port: 6379,
-      password: undefined,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS,
     },
   },
 } as ICacheConfig
