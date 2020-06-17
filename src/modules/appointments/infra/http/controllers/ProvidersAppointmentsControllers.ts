284009f56ea1c542f4843869edab92f8f9ce08c8
Vinicius Flores <viniciusflores379@gmail.com>
2020-06-17 19:00:33 +0000

    :zap: upgrade appointment repository

--- src/modules/appointments/infra/http/controllers/ProvidersAppointmentsControllers.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersAppointmentsControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
+import { classToClass } from 'class-transformer'
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService'
 
 export default class ProvidersAppointmentsControllers {
@@ -18,6 +19,6 @@ export default class ProvidersAppointmentsControllers {
       day: Number(day),
     })
 
-    return response.json(appointments)
+    return response.json(classToClass(appointments))
   }
 }
